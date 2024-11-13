0a68033643b50c50cf59abaa175fe8fd33acb971
uma <uma@tigeen.ch>
2024-11-13 10:20:47 +0530

    Removed unused component

--- src/navigation/navigation.jsx
+++ src/navigation/navigation.jsx
@@ -1,6 +1,5 @@
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Home from '../screens/home';
-import Timer from '../screens/timer';
 import Header from './header';
 
 const Stack = createNativeStackNavigator();
@@ -12,13 +11,6 @@ const NavigationRoutes = () => {
         header: props => <Header {...props} />,
       }}>
       <Stack.Screen name="Home" component={Home} />
-      <Stack.Screen
-        name="Timer"
-        component={Timer}
-        options={{
-          headerShown: false,
-        }}
-      />
     </Stack.Navigator>
   );
 };
